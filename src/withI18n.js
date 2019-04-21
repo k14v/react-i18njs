a957a2b8db25e3d9ef4b539f5418e4ff924cf2cb
Miguel Osorio @kelvur <Osorio10.92@gmail.com>
2019-04-21 23:37:06 +0200

    feat: use the function React.forwardRef in the wrapper

--- src/withI18n.js
+++ src/withI18n.js
@@ -6,16 +6,17 @@ import hoistStatics from 'hoist-non-react-statics';
 import useI18n from './useI18n';
 
 
-const addExtraProps = (mapI18nToProps = i18n => ({trls: i18n.trls}), props, i18n) => ({
+const addExtraProps = (mapI18nToProps = i18n => ({trls: i18n.trls}), props, i18n, ref) => ({
+  ref,
   ...props,
   ...mapI18nToProps(i18n),
 });
 
 export default mapI18nToProps => (Component) => {
-  const WrapperComponent = (props) => {
+  const WrapperComponent = React.forwardRef((props, ref) => {
     const i18n = useI18n();
-    return React.createElement(Component, addExtraProps(mapI18nToProps, props, i18n));
-  };
+    return React.createElement(Component, addExtraProps(mapI18nToProps, props, i18n, ref));
+  });
   WrapperComponent.displayName = `withI18n(${Component.displayName || Component.name})`;
   WrapperComponent.WrappedComponent = Component;
   return hoistStatics(WrapperComponent, Component);

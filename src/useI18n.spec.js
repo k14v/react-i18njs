57f28c6d5db2978a42853ac7ef28369d1572845b
Miguel Osorio @kelvur <Osorio10.92@gmail.com>
2020-03-30 23:17:05 +0200

    test: improve the readability and the code of the test files

--- src/useI18n.spec.js
+++ src/useI18n.spec.js
@@ -1,74 +1,29 @@
 // Core
 import React from 'react';
-import ReactDOM from 'react-dom';
-// Test
-import {act} from 'react-dom/test-utils';
+// Testing
 import test from 'ava';
-import {shallow} from 'enzyme';
-// I18n library
-import i18njs from '@k14v/i18njs';
-// Provider
+import { render, cleanup } from '@testing-library/react';
+import { fetchI18n } from './testUtils';
+// Components
 import I18nProvider from './I18nProvider';
-// Hooks
 import useI18n from './useI18n';
-// Constants
-import {I18N_EVENTS} from './constants';
 
 
-const locales = {
-  es: {
-    'Hello World': 'Hola Mundo',
-  },
-  en: {
-    'Hello World': 'Hello World',
-  },
-};
+test.afterEach(cleanup);
 
-const Provider = ({i18n, children}) => <I18nProvider i18n={i18n}>{children}</I18nProvider>;
-
-const Consumer = (_) => {
+const Consumer = ({word = 'Teléfono'}) => {
   const i18n = useI18n();
-  return (
-    <p>{i18n.trls.__('Hello World')}</p>
-  );
+  return <span>{i18n.trls.__(word)}</span>;
 };
 
-test.cb('it should render', (t) => {
-  t.timeout(10000);
-  t.plan(1);
-  const i18n = i18njs({locales, locale: 'es'});
-  i18n.subscribe(({type}) => {
-    if (type === I18N_EVENTS.LOADED) {
-      const wrapper = shallow(
-        <Provider i18n={i18njs({locales, locale: 'es'})}>
-          <Consumer />
-        </Provider>
-      );
-      t.true(wrapper.exists());
-      t.end();
-    }
-  });
+test('render without errors', async (t) => {
+  const i18n = await fetchI18n();
+  render(<I18nProvider i18n={i18n}><Consumer /></I18nProvider>);
+  t.pass();
 });
 
-test.cb('it should translate', (t) => {
-  t.timeout(10000);
-  t.plan(1);
-  const i18n = i18njs({locales, locale: 'es'});
-  i18n.subscribe(({type}) => {
-    if (type === I18N_EVENTS.LOADED) {
-      const container = document.createElement('div');
-      document.body.appendChild(container);
-      act(() => {
-        ReactDOM.render(
-          <Provider i18n={i18njs({locales, locale: 'es'})}>
-            <Consumer />
-          </Provider>,
-          container
-        );
-      });
-      const p = container.querySelector('p');
-      t.true(p.textContent === locales.en['Hello World']);
-      t.end();
-    }
-  });
+test('translates correctly', async (t) => {
+  const i18n = await fetchI18n();
+  const { getByText } = render(<I18nProvider i18n={i18n}><Consumer word="Perro" /></I18nProvider>);
+  t.truthy(getByText('Dog'));
 });
--- src/withI18n.spec.js
+++ src/withI18n.spec.js
@@ -1,66 +1,60 @@
 // Core
 import React, { useRef } from 'react';
-import ReactDOM from 'react-dom';
-// Test
-import {act} from 'react-dom/test-utils';
+// Testing
 import test from 'ava';
-import {mount} from 'enzyme';
+import { render, cleanup } from '@testing-library/react';
 import sinon from 'sinon';
-// I18n library
-import i18njs from '@k14v/i18njs';
-// Providers
+import { fetchI18n } from './testUtils';
+// Components
 import I18nProvider from './I18nProvider';
-// Decorators
+// HOC
 import withI18n from './withI18n';
 
 
-const BaseComponent = _ => <p>Hello World!</p>;
+test.afterEach(cleanup);
+
+const BaseComponent = ({trls, word = 'Perro'}) => <span>{trls.__(word)}</span>;
 
 const WrappedComponent = withI18n()(BaseComponent);
 
-const ProviderComponent = _ => (
-  <I18nProvider i18n={i18njs({locales: ['en', 'es', 'pt']})}>
-    <WrappedComponent />
-  </I18nProvider>
-);
-
-test('it should render', (t) => {
-  const warnSpy = sinon.spy(console, 'warn');
-  const wrapper = mount(<WrappedComponent />);
-  t.true(wrapper.exists());
-  warnSpy.restore();
+test('render without errors', (t) => {
+  const warnStub = sinon.stub(console, 'warn').callsFake(() => {});
+  render(<WrappedComponent />);
+  t.pass();
+  warnStub.restore();
 });
 
-test('it should render with I18nProvider', (t) => {
-  const wrapper = mount(<ProviderComponent />);
-  t.true(wrapper.exists());
+test('render with a provider', async (t) => {
+  const i18n = await fetchI18n();
+  render(<I18nProvider i18n={i18n}><WrappedComponent /></I18nProvider>);
+  t.pass();
 });
 
-test('it\'s forwarding the ref', (t) => {
+test('translates correctly', async (t) => {
+  const i18n = await fetchI18n();
+  const { getByText } = render(<I18nProvider i18n={i18n}><WrappedComponent word="Teléfono" /></I18nProvider>);
+  t.truthy(getByText('Phone'));
+});
+
+test('forwards the ref', async (t) => {
   const INITIAL_VALUE = 'Popeye';
   const Inner = React.forwardRef((props, ref) => {
-    t.truthy(ref);
-    t.true(ref.current === INITIAL_VALUE);
+    t.is(ref.current, INITIAL_VALUE);
     return (
       <button ref={ref} {...props} type="button" />
     );
   });
   const Wrapper = withI18n()(Inner);
-  const Provider = (props) => {
+  const Provider = ({i18n}) => {
     const ref = useRef(INITIAL_VALUE);
     return (
-      <I18nProvider i18n={i18njs({locales: ['en', 'es', 'pt']})}>
+      <I18nProvider i18n={i18n}>
         <Wrapper ref={ref} />
       </I18nProvider>
     );
   };
-  const container = document.createElement('div');
-  document.body.appendChild(container);
-  act(() => {
-    ReactDOM.render(
-      <Provider />,
-      container,
-    );
-  });
-  document.body.removeChild(container);
+
+  const i18n = await fetchI18n();
+
+  render(<Provider i18n={i18n} />);
 });
